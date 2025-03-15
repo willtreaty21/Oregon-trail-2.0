44f3616a1eadb97eee2b1637990081b627f6c394
willtreaty21 <BaughmOJ27@pwcs-edu.org>
2025-03-15 08:08:23 -0400

    stuff

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 
 int doneShopping = 0;
@@ -15,8 +16,18 @@ int food = 0;
 int clothing = 0;
 int ammunition = 0;
 
+
+// travel info
+int city;
 int leftAt;        // 0 = march, 1 = april, 2 = may, 3 = june, 4 = july 
 int Class;         // 0 =  farmer, 1 = carpenter, 2 = banker
+int date = 1;
+int month = 1; //1-12 normal months
+int weather = 1;       // 1 = clear sky, 2 = cloudy, 3 = rainy, 4 = lightly rainy, 5 = thunderstorm, 6 = tornado, 7 = sandstorm
+
+//
+int distanceTraveled = 0;
+int rand2;
 
 // spare parts
 int wagonWheel;
@@ -32,6 +43,43 @@ char character5[20];
 void openSavedGame() {
     printf("doesn't work yet");
 }
+
+int returnRandom(int max) { // make int to return a random number
+	// return with random c function
+    return rand() % (max + 1);
+}
+
+void travel() {
+
+}
+
+void checkSupplies() {
+	printf("\033[2JYou have:\n$%d\n%d Oxen\n%d Pounds of Food\n%d Spare Sets of Clothing\n%d Rounds of Ammunition\n%d Wagon Wheels\n%d Wagon Axles \n%d Wagon Tongues\n", cash, oxen, food, clothing, ammunition, wagonWheel, wagonAxle, wagonTongue);
+}
+
+void map() {
+
+}
+
+void changePace() {
+
+}
+
+void changeRations() {
+
+}
+
+void attemptToRest() {
+	printf("\033[2JYou attempt to rest\n");
+	//rand2 = returnRandom(100);
+	// persentage table for randoms
+	// 1 - 55 you rest successfully
+	// 56 - 65 your oxen are stolen ( run random for ammount) 
+	// 66 - 70 You are attcked by native americans
+	
+	
+}
+
 
 void setCharacterNames() {
     printf("\033[2JPlease input a name for the leader\n");
@@ -261,7 +309,129 @@ void gameLoop() {
         //  4. Change Pace
         //  5. change food rations
         //  6. attempt to rest
-        printf("")
+        switch(city) { // print city/if on the trail print that
+            case 1:
+                printf("                    Independence, Missouri\n");
+                break;
+            case 2:
+                printf("                    Chimney Rock, Nebraska");
+                break;
+            case 3:
+                printf("                    Scotts Bluff, Nebraska");
+                break;
+            case 4:
+                printf("                    Fort Laramie, Wyoming");
+                break;
+       	    case 5:
+                printf("                  Independence Rock, Wyoming");
+                break;
+            case 6:
+                printf("                     South Pass, Wyoming");
+                break;
+            case 7:
+                printf("                 Three Island Crossing, Idaho");
+                break;
+            case 8:
+                printf("                       Fort Hall, Idaho");
+                break;
+            case 9:
+                printf("                      Oregon City, Oregon");
+                break;
+        }
+        switch(month) { // print month
+            case 1:
+                printf("                         January");
+                break;
+            case 2:
+                printf("                        February");
+                break;
+            case 3:
+                printf("                           March");
+                break;
+            case 4:
+                printf("                           April");
+                break;
+            case 5:
+                printf("                             May");
+                break;
+            case 6:
+                printf("                            June");
+                break;
+            case 7:
+                printf("                            July");
+                break;
+            case 8:
+                printf("                          August");
+                break;
+            case 9:
+                printf("                       September");
+                break;
+            case 10:
+                printf("                         October");
+                break;
+            case 11:
+                printf("                        November");
+                break;
+            case 12:
+                printf("                        December");
+                break;
+            default:
+                printf("                           March");
+                break;
+        }
+        switch(date) { // print date
+            case 1:
+                printf("1st,\n");
+                break;
+            case 2:
+                printf("2nd,\n");
+                break;
+            case 3:
+                printf("3rd,\n");
+                break;
+            case 21:
+                printf("21st,\n");
+                break;
+            case 22:
+                printf("22nd,\n");
+                break;
+            case 23:
+                printf("23rd,\n");
+                break;
+            case 31:
+                printf("31st,\n");
+                break;
+            default:
+                printf("%dth,\n", date);
+                break;
+        }
+        switch(weather) { // 1 clear skies, 2 cloudy, 3 rainy, 4 lightly rainy, 5 thunderstorm, 6 tornado, 7 sandstorm
+            case 1:
+            printf("\n                     Weather: Clear Skies\n");
+            break;
+            case 2:
+	    printf("\n                       Weather: Cloudy\n");
+            break;
+            case 3:
+	    printf("\n                       Weather: Rainy");
+            break;
+            case 4:
+	    printf("\n                   Weather: Lightly Rainy");
+            break;
+            case 5:
+	    printf("\n                   Weather: Thunderstorming")
+            break;
+            case 6:
+	    printf("\n                      Weather: Tornado");
+            break;
+            case 7:
+            break;
+        }
+
+        printf("What would you like to do?\n1. Travel The Trail\n2. Check Supplies\n3. Look at Map\n4. Change Pace\n5. Modify Rations\n6. Attempt to Rest\n");
+	scanf(" %d", &playerInput);
+	(playerInput > 4)? (playerInput == 6)? printf("resting\n") : printf("Checking Rations\n"): (playerInput > 2)? (playerInput == 4) ? printf("Changing Pace\n") : printf("Checking Map\n") : (playerInput == 2) ? checkSupplies() : printf("Traveiling the trail\n");
+	scanf(" %c", playerCharInput);
     }
 }
 
@@ -273,7 +443,7 @@ void newGame() {
     case 1:
         printf("/033[2JFarmer Chosen\n\033[1;37mPress any letter than enter to continue\033[0;37m\n");
         Class = 0;
-        cash = 400.00;
+        cash = 600.00;
         break;
     case 2:
         printf("/033[2JCarpenter Chosen\n\033[1;37mPress any letter than enter to continue\033[0;37m\n");
@@ -364,7 +534,8 @@ int main() {
     printf("Welcome to a remake of Oregon Trail\nWould you like to start a new game(1) or open a saved one(2)?(1/2)\n");
     // scanf(" %d", &playerInput);
     // (playerInput == 1)? newGame(): openSavedGame();
-    shop();
+    //shop();
+    gameLoop();
 
     return 0;
 }
